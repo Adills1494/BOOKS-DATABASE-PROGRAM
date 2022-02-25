e4f6780c5b08f4b7944ffca7a5dd41edfeffdde9
Adills1494 <79712908+Adills1494@users.noreply.github.com>
2022-02-24 20:53:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from ast import Expression
-from models import (Base, session, 
-                    Book, engine)
+from models import (Base, session, Book, engine)
+
 import datetime   
 import csv
 import time
@@ -22,13 +22,30 @@ def  menu():
             input('''
                   \rPlease choose one of the options above.
                   \rA number from 1-5.
-                  \rPress enter to try again.''')
+                  \rPress Enter to try again.''')
 
 
-def clean_date(date_str):
+def submenu():
+    while True:
+        print('''
+            \r1) Edit
+             \r2) Delete
+             \r3) Return to Main Menu
+             ''')
+        choice = input('What would you like to do?  ')
+        if choice in ['1', '2', '3']:
+            return choice
+        else:
+            input('''
+                  \rPlease choose one of the options above.
+                  \rA number from 1-3.
+                  \rPress Enter to try again.''')                   
+
+
+def clean_date(date_str):        
     months = ['January', 'February', 'March', 'April', 'May', 'June', 
               'July', 'August', 'September', 'October', 'November', 'December']
-    split_date = date_str.split('  ')
+    split_date = date_str.split(' ')
     try:
         month = int(months.index(split_date[0]) + 1)
         day = int(split_date[1].split(',')[0])
@@ -39,7 +56,7 @@ def clean_date(date_str):
               \n****** DATE ERROR ******
               \rThe date format should include a valid Month day, year from the past.
               \rEx: January 13, 2003
-              \rPress enter to try again.
+              \rPress Enter to try again.
               \r************************''')
         return
     else:
@@ -54,14 +71,58 @@ def clean_price(price_str):
               \n****** PRICE ERROR ******
               \rThe date format should be a number without a currency symbol.
               \rEx: 10.99
-              \rPress enter to try again.
+              \rPress Enter to try again.
               \r************************''')
         return
     else:
         return int(price_float * 100)
-    
+ 
+ 
+def clean_id(id_str, options):
+    try:
+        book_id = int(id_str)
+    except ValueError:
+        input('''
+              \n****** ID ERROR ******
+              \rThe ID format should be a number.
+              \rPress Enter to try again.
+              \r************************''') 
+        return
+    else:
+        if book_id in options:
+            return book_id
+        else:
+            input('''
+              \n****** ID ERROR ******
+              \rOptions: {options}
+              \rPress Enter to try again.
+              \r************************''') 
+            return   
+ 
 
-
+def edit_check(column_name, current_value):
+    print(f'\n*** EDIT {column_name} ***')
+    if column_name == 'Price':
+        print(f'\rCurrent Value: {current_value/100}')
+    elif column_name == 'Date':
+            print(f'\rCurrent Value: {current_value.strftime("%B %D, %Y")}')
+    else:
+        print(f'\rCurrent Value: {current_value}')
+     
+    if column_name == 'Date' or column_name == 'Price':
+        while True:
+            changes = input('What would you like to change the value to? ')
+            if column_name == 'Date':
+                changes = clean_date(changes)
+                if type(changes) == datetime.date:
+                    return changes
+            elif column_name == 'Price':
+                changes = clean_price(changes)
+                if type(changes) == int:
+                    return changes
+    else:
+        return input('What would you like to change the value to? ')
+     
 def add_csv():
     with open('suggested_books.csv') as csvfile:
         data = csv.reader(csvfile)
@@ -74,9 +135,9 @@ def add_csv():
                 price = clean_price(row[3])
                 new_book = Book(title = title, author = author, published_date = date, price = price)
                 session.add(new_book)
-        session.commit()    
- 
-
+        session.commit()  
+        
+        
 def app():
     app_running = True
     while app_running:
@@ -96,7 +157,7 @@ def app():
                 price = clean_price(price)
                 if type(price) == int:
                     price_error = False
-            new_book = Book(title = title, author = author, published_date = date, price = price)
+            new_book = Book(title=title, author=author, published_date=date, price=price)
             session.add(new_book)
             session.commit()
             print('Book Added!')
@@ -104,13 +165,52 @@ def app():
         elif choice == '2': 
             for book in session.query(Book):
                 print(f'{book.id} | {book.title} | {book.author}')
-            input('\nPress enter to return to the main menu.')    
+            input('\nPress Enter to return to the Main Menu.')    
         elif choice == '3':
-            
-            pass
+            id_options = []
+            for book in session.query(Book):
+                id_options.append(book.id)
+            id_error = True
+            while id_error:    
+                id_choice = input(f'''
+                    \nId options:  {id_options}
+                    \rBook ID: ''')   
+                id_choice = clean_id(id_choice, id_options)
+                if type(id_choice) == int:    
+                    id_error = False 
+            the_book = session.query(Book).filter(Book.id == id_choice).first()
+            print(f'''
+                  \n{the_book.title} by {the_book.author}
+                  \rPublished: {the_book.published_date}
+                  \rPrice:  ${the_book.price / 100}
+                  ''')
+            sub_choice = submenu()
+            if sub_choice == '1':
+                the_book.title = edit_check('Title', the_book.title)
+                the_book.author = edit_check('Author', the_book.author)
+                the_book.published_date = edit_check('Date', the_book.published_date)
+                the_book.price = edit_check('Price', the_book.price)
+                session.commit()
+                print('Book updated')
+                time.sleep(1.5)
+            elif sub_choice == '2':
+                session.delete(the_book)
+                session.commit()
+                print('Book Deleted!')
+                time.sleep(1.5)
         elif choice == '4':
             
-            pass
+            oldest_book = session.query(Book).order_by(Book.published_date).first()
+            newest_book = session.query(Book).order_by(Book.published_date.desc()).first()
+            total_books = session.query(Book).count()
+            python_books = session.query(Book).filter(Book.title.like('%Python%'))
+            print('''
+                  \n***** BOOK ANALYSIS *****
+                  \rOldest Book: {oldest_book}
+                  \rNewest Book: {newest_book}
+                  \rTotal Books: {total_books}
+                  \rNumber of Python Books: {python_books}''')
+            input('\nPress Enter to return to the Main Menu.')
         else:
             print('GOODBYE')
             app_running = False
@@ -120,6 +220,3 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)    
     add_csv()
     app() 
-    
-    for book in session.query(Book):
-        print(book)
